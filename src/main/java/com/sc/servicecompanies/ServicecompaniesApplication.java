9288c03033b3f472eb4b51c3bbfe0c1759ce472d
DannaG18 <158192981+DannaG18@users.noreply.github.com>
2024-10-08 11:54:44 -0500

    feat: :tada: Project Creation

---
+++ src/main/java/com/sc/servicecompanies/ServicecompaniesApplication.java
@@ -0,0 +1,13 @@
+package com.sc.servicecompanies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicecompaniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicecompaniesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc/servicecompanies/ServicecompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.servicecompanies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicecompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
