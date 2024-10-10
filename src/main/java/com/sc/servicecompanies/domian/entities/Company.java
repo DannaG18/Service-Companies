c4007760949f4f25c360824e13ed812b580c00b6
JhonJMD <jjmdcampus@gmail.com>
2024-10-10 11:10:34 -0500

    build: :construction: Work in progress

---
+++ src/main/java/com/sc/servicecompanies/domian/entities/Company.java
@@ -0,0 +1,24 @@
+package com.sc.servicecompanies.domian.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "company")
+@Data
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int companyId;
+
+    @Column(nullable = false)
+    private int companyTypeId;
+
+    @Column(length = 100, nullable = false)
+    private String nameCompany;
+}
