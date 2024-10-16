b6611052642054331c02f16a85f1c89a8513d26a
DannaG18 <158192981+DannaG18@users.noreply.github.com>
2024-10-16 10:40:18 -0500

    feat: :sparkles: User, Roles and Security done

---
+++ src/main/java/com/sc/servicecompanies/infrastructure/repositories/users/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sc.servicecompanies.infrastructure.repositories.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sc.servicecompanies.domain.entities.login.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+}
