38a51bb9270e2b22b371585c9a0a0e379aebf82e
JhonJMD <jjmdcampus@gmail.com>
2024-10-27 22:08:42 -0500

    fix: :memo: Update SQL

--- src/main/resources/security.sql
+++ src/main/resources/security.sql
@@ -1,40 +1,47 @@
--- CREACIÓN DE MODULOS
+-- CREACIÓN DE MÓDULOS
 INSERT INTO module (name, base_path) VALUES ('CUSTOMER', '/customers');
+SET @module_customer_id = LAST_INSERT_ID();
+
 INSERT INTO module (name, base_path) VALUES ('AUTH', '/auth');
+SET @module_auth_id = LAST_INSERT_ID();
 
--- CREACIÓN DE OPERACIONES
+-- CREACIÓN DE OPERACIONES, usando las variables de módulo para el campo module_id
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ALL_CUSTOMERS','', 'GET', false, @module_customer_id);
+SET @operation_read_all_customers_id = LAST_INSERT_ID();
 
-INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ALL_CUSTOMERS','', 'GET', false, 3);
-INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('REGISTER_ONE','', 'POST', true, 3);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('REGISTER_ONE','', 'POST', true, @module_customer_id);
+SET @operation_register_one_id = LAST_INSERT_ID();
 
-INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('AUTHENTICATE','/authenticate', 'POST', true, 4);
-INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('VALIDATE-TOKEN','/validate-token', 'GET', true, 4);
-INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_MY_PROFILE','/profile','GET', false, 4);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('AUTHENTICATE','/authenticate', 'POST', true, @module_auth_id);
+SET @operation_authenticate_id = LAST_INSERT_ID();
+
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('VALIDATE-TOKEN','/validate-token', 'GET', true, @module_auth_id);
+SET @operation_validate_token_id = LAST_INSERT_ID();
+
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_MY_PROFILE','/profile','GET', false, @module_auth_id);
+SET @operation_read_my_profile_id = LAST_INSERT_ID();
 
 -- CREACIÓN DE ROLES
 INSERT INTO role (name) VALUES ('CUSTOMER');
+SET @role_customer_id = LAST_INSERT_ID();
+
 INSERT INTO role (name) VALUES ('ASSISTANT_ADMINISTRATOR');
+SET @role_assistant_admin_id = LAST_INSERT_ID();
+
 INSERT INTO role (name) VALUES ('ADMINISTRATOR');
+SET @role_admin_id = LAST_INSERT_ID();
 
--- CREACIÓN DE PERMISOS
-INSERT INTO granted_permission (role_id, operation_id) VALUES (1, 15);
+-- CREACIÓN DE PERMISOS usando las variables de ID generadas previamente
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_customer_id, @operation_read_my_profile_id);
 
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 1);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 2);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 4);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 6);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 7);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 9);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 15);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_assistant_admin_id, @operation_read_all_customers_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_assistant_admin_id, @operation_register_one_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_assistant_admin_id, @operation_authenticate_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_assistant_admin_id, @operation_validate_token_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_assistant_admin_id, @operation_read_my_profile_id);
 
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 1);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 2);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 3);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 4);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 5);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 6);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 7);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 8);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 9);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 10);
-INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 15);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_admin_id, @operation_read_all_customers_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_admin_id, @operation_register_one_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_admin_id, @operation_authenticate_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_admin_id, @operation_validate_token_id);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (@role_admin_id, @operation_read_my_profile_id);
